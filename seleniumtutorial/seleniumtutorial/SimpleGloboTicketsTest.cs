684093073db9e36057be9283634fa9ea7ea0a5c5
sadik312 <sadikislam46@gmail.com>
2023-08-28 23:37:16 +0100

    created NUnit test

---
+++ seleniumtutorial/seleniumtutorial/SimpleGloboTicketsTest.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seleniumtutorial
+{
+    internal class SimpleGloboTicketsTest
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            // 1. Start web browser
+            var driver = new ChromeDriver();
+            // Set window size
+            driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
+        }
+    }
+}
