34ee7242729f19d43fdf2f57fd339c125e753b04
sadik312 <sadikislam46@gmail.com>
2023-08-28 23:52:49 +0100

    created simple test to add name and click done

--- seleniumtutorial/seleniumtutorial/SimpleGloboTicketsTest.cs
+++ seleniumtutorial/seleniumtutorial/SimpleGloboTicketsTest.cs
@@ -1,9 +1,12 @@
 ﻿using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using OpenQA.Selenium;
 
 namespace seleniumtutorial
 {
@@ -16,6 +19,14 @@ namespace seleniumtutorial
             var driver = new ChromeDriver();
             // Set window size
             driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
+            // 2. Pass through URL for application to be tested
+            driver.Navigate().GoToUrl("http://localhost:4200");
+            // 3. Locate Name input and Done button and enter input
+            driver.FindElement(By.Id("fullName")).SendKeys("Josh Smith");
+            driver.FindElement(By.Id("add-btn")).Click();
+
+            // 4. Terminate driver
+            driver.Quit();
         }
     }
 }
