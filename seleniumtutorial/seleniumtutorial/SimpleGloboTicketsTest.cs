eb5ec18d901fd906bcbde773b97fc4c76f0c3a9e
sadik312 <sadikislam46@gmail.com>
2023-08-29 10:42:20 +0100

    created test using relative locators

--- seleniumtutorial/seleniumtutorial/SimpleGloboTicketsTest.cs
+++ seleniumtutorial/seleniumtutorial/SimpleGloboTicketsTest.cs
@@ -18,14 +18,34 @@ namespace seleniumtutorial
             // 1. Start web browser
             var driver = new ChromeDriver();
             // Set window size
-            driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
+            driver.Manage().Window.Size = new Size(1920, 1080);
             // 2. Pass through URL for application to be tested
             driver.Navigate().GoToUrl("http://localhost:4200");
             // 3. Locate Name input and Done button and enter input
             driver.FindElement(By.Id("fullName")).SendKeys("Josh Smith");
             driver.FindElement(By.Id("add-btn")).Click();
 
+            // Relvative Locator
+            driver.FindElement(RelativeBy
+                .WithLocator(By.TagName("textarea"))
+                .Below(By.TagName("full-name")))
+                .SendKeys("Something important");
+
             // 4. Terminate driver
+            // driver.Quit();
+        }
+        [Test]
+        public void UsingRelativeLocators()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Size = new Size(1920, 1080);
+            driver.Navigate().GoToUrl("http://localhost:4200");
+            // use relative locator
+            driver.FindElement(RelativeBy
+                .WithLocator(By.TagName("textarea"))
+                .Below(By.TagName("full-name")))
+                .SendKeys("Something important");
+
             driver.Quit();
         }
     }
