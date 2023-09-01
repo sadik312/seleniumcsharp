4a66f6b6fe013b1ca60d201a15fbb90c55bcad61
sadik312 <sadikislam46@gmail.com>
2023-09-01 21:28:49 +0100

    refactored code for multiple tests in one file

--- seleniumtutorial/seleniumtutorial/SimpleGloboTicketsTest.cs
+++ seleniumtutorial/seleniumtutorial/SimpleGloboTicketsTest.cs
@@ -10,30 +10,34 @@ using OpenQA.Selenium;
 
 namespace seleniumtutorial
 {
+    [TestFixture]
     internal class SimpleGloboTicketsTest
     {
+        private IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            // initialise webdriver and set window size
+            driver = new ChromeDriver();
+            driver.Manage().Window.Size = new Size(1920, 1080);
+            // navigate to url of test app
+            driver.Navigate().GoToUrl("http://localhost:4200");
+        }
         [Test]
         public void SimpleTest()
         {
-            // 1. Start web browser
-            var driver = new ChromeDriver();
-            // Set window size
-            driver.Manage().Window.Size = new Size(1920, 1080);
-            // 2. Pass through URL for application to be tested
-            driver.Navigate().GoToUrl("http://localhost:4200");
-            // 3. Locate Name input and Done button and enter input
+            // Locate Name input and Done button and enter input
             driver.FindElement(By.Id("fullName")).SendKeys("Josh Smith");
             driver.FindElement(By.Id("add-btn")).Click();
 
-            // Relvative Locator
+            // Relative Locator
             driver.FindElement(RelativeBy
                 .WithLocator(By.TagName("textarea"))
                 .Below(By.TagName("full-name")))
                 .SendKeys("Something important");
 
-            // 4. Terminate driver
-            // driver.Quit();
         }
+        // Test using Relative Locators
         [Test]
         public void UsingRelativeLocators()
         {
@@ -46,6 +50,18 @@ namespace seleniumtutorial
                 .Below(By.TagName("full-name")))
                 .SendKeys("Something important");
 
+            // Select multiple elements using 'FindElements' method
+            driver.FindElements(RelativeBy
+                .WithLocator(By.CssSelector("input[type='checkbox']"))
+                .Above(By.Id("add-btn")))
+                .First()
+                .Click();
+
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            // clean up resources
             driver.Quit();
         }
     }
